3d684c3edf62ad6487001a256b016eaaa42d39b5
Sujan Khatri <falconhawk@protonmail.com>
2019-02-13 22:14:30 +0900

    Destructuting Assignment for nested objects

---
+++ Destructuring_Assignment_Nestsed_Objects.js
@@ -0,0 +1,24 @@
+//Snippet
+const a = {
+    start: { x: 5, y: 6},
+    end: { x: 6, y: -9 }
+  };
+  const { start : { x: startX, y: startY }} = a;
+  console.log(startX, startY); // 5, 6
+
+
+  //main program
+
+
+  const LOCAL_FORECAST = {
+    today: { min: 72, max: 83 },
+    tomorrow: { min: 73.3, max: 84.6 }
+  };
+  
+  function getMaxOfTmrw(forecast) {
+    "use strict";
+    const {tomorrow : { max : maxOfTomorrow }} = forecast;
+    return maxOfTomorrow;
+  }
+  
+  console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
